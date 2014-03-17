9cb7133e613f8a54f503ad1a1075fb820491da72
dburrell <git@ravensoft.org>
2014-03-17 18:00:21 +0000

    Camera movement locked. Draws multiple objects.
    "objects" array contains all objects to be drawn, this is cycled instead
    of just drawing the player now.
    Camera movement now works and doesn't reset on second run.
    Camera movement can lock controls during animation (for cutscenes etc)
    Control lock is implementable - possibly useful for cutscenes or
    pausing. IF USED for pausing, then need to allow the pause button to be
    pressed, obviously.

--- jumper.js
+++ jumper.js
@@ -1,103 +1,158 @@
-var objects = new Object();
+var objects = new Array();
 
 var player = 
-    {
-      x0: 0,
-      y0: 400,
-      x: 0,
-      y: 400,
-      height: 20,
-      width: 20,
-      dy: 0,
-      dx: 0,
-      right: false,
-      left: false,
-
-      speed: 700,
-
-      jumpCount:0,
-      jumping:false,      
-      jumpPower:500,
-      j0:800,
-
-      touchingWall:false,
-
-      falling:true,
-
-      xstart: new Date().getTime(), 
-      fstart: new Date().getTime(),
-      jstart: new Date().getTime()
-    }
-
-var env = 
-
-    {      
-      gravity: 200,
-      keydown: false,      
-      startTime: new Date().getTime(), 
-      endTime: new Date().getTime(),
-      left: false,
-      right: false,
-      up: false,
-      down: false,
-      y0: 0,      
-      x0: 0,
-      y: 0,
-      x: 0,
-      endY: 0,
-      endX: 0,
-
-      moving: false,
-
-      moveCamera: function(x, y, t)
-      {        
-        env.startTime = new Date().getTime();
-        env.endTime = env.startTime + (t * 1000);  
-        env.y0 = env.y;
-        env.x0 = env.x;                    
-        env.endY = y;
-        env.endX = x;
-        env.moving = true;       
-      },
-
-      setCameraPosition: function()
-      {        
-        if (env.moving)
-        {                             
-          env.y = (env.endY - env.y0) * env.getFrac();  
-          env.x = (env.endX - env.x0) * env.getFrac();                     
-        }        
-      },
-
-      getFrac: function()
-      {
-        var now = new Date().getTime() ;  
-        var timeSoFar = now - env.startTime;
-        var totalTime = env.endTime - env.startTime;       
-        var frac = timeSoFar/totalTime;
-        if (now >= env.endTime)
-        { env.moving = false; }
-        return frac;
-      }      
-    }
-
-
-function addObj(o)
 {
-  n = Object.keys(objects).length
-  objects[n] = o;   
+  x0: 0,
+  y0: 400,
+  x: 0,
+  y: 400,
+  height: 20,
+  width: 20,
+  dy: 0,
+  dx: 0,
+  right: false,
+  left: false,
+
+  speed: 700,
+  color:"#000",
+
+  jumpCount:0,
+  maxJumps:2,
+  jumping:false,      
+  jumpPower:500,
+  j0:800,
+
+  touchingWall:false,
+
+  falling:true,
+
+  xstart: new Date().getTime(), 
+  fstart: new Date().getTime(),
+  jstart: new Date().getTime()
 }
 
+
+var camera = 
+{
+  startTime: new Date().getTime(), 
+  endTime: new Date().getTime(),
+  y0: 0,      
+  x0: 0,
+  endY: 0,
+  endX: 0,
+  y: 0,
+  x: 0,  
+  moving: false,
+  controlsLocked: false,
+
+  move: function(x, y, t, lockControls)
+  {        
+    if (lockControls)
+    {
+      camera.controlsLocked = true;
+      env.controlsLocked++;
+    }
+    camera.startTime = new Date().getTime();
+    camera.endTime = camera.startTime + (t * 1000);  
+    camera.y0 = camera.y;
+    camera.x0 = camera.x;                    
+    camera.endY = y;
+    camera.endX = x;
+    camera.moving = true; 
+    renderplayer();
+  },
+
+  setPosition: function()
+  {        
+    if (camera.moving)
+    {                             
+      camera.y = camera.y0 + ((camera.endY - camera.y0) * camera.getFrac());  
+      camera.x = camera.x0 + ((camera.endX - camera.x0) * camera.getFrac());                     
+    }        
+  },
+
+  getFrac: function()
+  {
+    var now = new Date().getTime() ;  
+    var timeSoFar = now - camera.startTime;
+    var totalTime = camera.endTime - camera.startTime;       
+    var frac = timeSoFar/totalTime;
+    if (now >= camera.endTime)
+    { 
+      camera.moving = false; 
+      if (camera.controlsLocked)
+      {
+        camera.controlsLocked = false;
+        env.controlsLocked--;
+      }
+    }
+    return frac;
+  }     
+}
+
+
+
+var env = 
+{      
+  gravity: 200,
+  keydown: false,      
+  rowLogs: 10,
+  controlsLocked: 0
+}
+
+
+/////////////////////////////////////////////////
+//Make a generic object (tree or enemy)
+/////////////////////////////////////////////////
+function newObj()
+{
+  var o = 
+  {
+    height: 20,
+    width: 20,
+    x: 0,
+    y: 0,
+    y0: 0,
+    x0: 0,
+    y0: 0,      
+    x0: 0,
+    y: 0,
+    x: 0,
+    endY: 0,
+    endX: 0,
+    speed: 700,
+    color: "#00F"
+  }
+  
+  return o;
+}
+
+function addObj(o)
+{  
+  objects[objects.length] = o;   
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//Initalise things
+///////////////////////////////////////////////////////////////////////////////
+
+//Add the player
 addObj(player);
+
+var a = newObj();
+addObj(a);
 
 
 
 var floor = 500;
 
 
+
 function log(m)
 {  
-  $("#log2").html($("#log").html() + "<br>" + $("#log2").html());
+  $("#log2").html($("#log").html() + "\n" + $("#log2").html());
   $("#log").html(m); 
 }
 
@@ -115,7 +170,7 @@ var renderplayer = function()
   if (player.left)  { player.dx = 0-player.speed;}
   player.x = player.x0 + (xduration * player.dx);  
 
-  env.setCameraPosition();
+  camera.setPosition();
   gravity(end); 
 
   showDebug();
@@ -124,11 +179,23 @@ var renderplayer = function()
   var context = c.getContext('2d');
   context.clearRect(0,0,canvas.width,canvas.height);
 
-  //Draw player
-  context.beginPath();
-  context.rect(player.x - env.x, player.y - env.y, player.width, player.height);
-  context.fillStyle = '#000000';  
-  context.fill();
+  //Draw objects (including player)
+  
+  for (var i = 0; i < objects.length; i++)
+  {    
+    var o = objects[i];    
+    context.beginPath();
+    context.rect(o.x - camera.x, o.y - camera.y, o.width, o.height);
+    context.fillStyle = o.color;//'#000000';  
+    context.fill();  
+  }
+  
+
+
+  //context.beginPath();
+  //context.rect(player.x - camera.x, player.y - camera.y, player.width, player.height);
+  //context.fillStyle = '#000000';  
+  //context.fill();
 
   //Draw floor
   context.beginPath();
@@ -137,7 +204,7 @@ var renderplayer = function()
   context.stroke();
 
   //Refresh
-  if (player.right || player.left || player.up || player.down || player.falling || player.jumping || env.moving)
+  if (player.right || player.left || player.up || player.down || player.falling || player.jumping || camera.moving)
   { requestAnimationFrame(renderplayer); }
 }
 
@@ -234,82 +301,87 @@ window.addEventListener( "keyup", doKeyUp, false )
 
 function doKeyDown(e) 
 { 
-  switch (e.keyCode)
+  if (env.controlsLocked === 0)
   {
+    switch (e.keyCode)
+    {
 
-    case 65:      
-      if (player.jumpCount < 2)
-      {       
-        log("keypress: jump (a)");
-        player.jumpCount++;        
-        player.jstart = new Date().getTime();
-        player.fstart = new Date().getTime();
-        player.y0 = player.y;
-        player.jumping = true;        
-        renderplayer();        
-      }
-      break;
-      
-    case 37:      
-      if (player.left === false && player.right === false) { pressLeft(); }      
-      if (player.right) { leftRightWait = "left"; }
-      break;
+      case 65:      
+        if (player.jumpCount < player.maxJumps)
+        {       
+          log("keypress: jump (a)");
+          player.jumpCount++;        
+          player.jstart = new Date().getTime();
+          player.fstart = new Date().getTime();
+          player.y0 = player.y;
+          player.jumping = true;        
+          renderplayer();        
+        }
+        break;
+        
+      case 37:      
+        if (player.left === false && player.right === false) { pressLeft(); }      
+        if (player.right) { leftRightWait = "left"; }
+        break;
 
-    case 39:      
-      if (player.left === false && player.right === false) { pressRight(); }
-      if (player.left) { leftRightWait = "right"; }
-      break;
+      case 39:      
+        if (player.left === false && player.right === false) { pressRight(); }
+        if (player.left) { leftRightWait = "right"; }
+        break;
 
-    case 84:
-      log("TEST: moving camera");
-      env.moveCamera(200,200,2);
-      break;
+      case 84:
+        log("TEST: moving camera");
+        camera.move(200,200,2, true);
+        break;
 
 
-    case 82:
-      //RESET
-      log("keypress: r");
-      player.y0 = 0;
-      player.x0 = 0;
-      player.x = 0;
-      player.y = 0;
-      player.right = false;
-      player.down = false;
-      ystart = new Date().getTime();
-      xstart = new Date().getTime();
-      renderplayer();      
-      break;      
-  }    
+      case 82:
+        //RESET
+        log("keypress: r");
+        player.y0 = 0;
+        player.x0 = 0;
+        player.x = 0;
+        player.y = 0;
+        player.right = false;
+        player.down = false;
+        ystart = new Date().getTime();
+        xstart = new Date().getTime();
+        renderplayer();      
+        break;      
+    }   
+  } 
 }
 
 
 function doKeyUp(e) 
 {   
-  switch (e.keyCode)
-  {        
-    case 37:
-      if (leftRightWait == "left")  { log("releasing left"); leftRightWait = ""; }
-      
-      if (player.left)
-      {
-        player.left = false;
-        player.x0 = player.x;        
-        if (leftRightWait === "right") { pressRight(); }
-      }
-      break;
-      
-      
-    case 39:
-      if (leftRightWait == "right")  { leftRightWait = ""; }
-      
-      if (player.right)
-      {
-        player.right = false;
-        player.x0 = player.x;        
-        if (leftRightWait === "left")   { pressLeft(); }
-      }
-      break;
-
+  if (env.controlsLocked === 0)
+  {
+    switch (e.keyCode)
+    {        
+      case 37:
+        if (leftRightWait == "left")  { log("releasing left"); leftRightWait = ""; }
+        
+        if (player.left)
+        {
+          player.left = false;
+          player.x0 = player.x;        
+          if (leftRightWait === "right") { pressRight(); }
+        }
+        break;
+        
+        
+      case 39:
+        if (leftRightWait == "right")  { leftRightWait = ""; }
+        
+        if (player.right)
+        {
+          player.right = false;
+          player.x0 = player.x;        
+          if (leftRightWait === "left")   { pressLeft(); }
+        }
+        break;
+    }
   }
 }
 
@@ -336,8 +408,13 @@ function showDebug()
   s += addRow("jumpCount",player.jumpCount);
   s += addRow("falling",player.falling);
   s += addRow("jstart",player.jstart);
-  s += addRow("env.y",env.y);
-  s += addRow("env.x",env.x);
+  s += addRow("camera.y",camera.y);
+  s += addRow("camera.x",camera.x);
+  s += addRow("camera.y0",camera.y0);
+  s += addRow("camera.x0",camera.x0);
+  s += addRow("camera.frac",camera.getFrac());
+  s += addRow("camera.moving",camera.moving);
+  s += addRow("env.controlsLocked",env.controlsLocked);
   s += addRow("leftRightWait",leftRightWait);
   
 
